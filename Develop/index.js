36b286f47204fc556be88e0022225125c4906677
BradMW <bodell94@yahoo.com>
2021-10-13 20:16:44 -0500

    Finished up questions and got the files to print. woking on format

--- Develop/index.js
+++ Develop/index.js
@@ -6,37 +6,62 @@ const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
 const questions = [
-    inquirer
-     .prompt([
+        {
+            type: 'input',
+             message: 'What is the name of your project?',
+             name: 'project_name',
+            },
         {
          type: 'input',
           message: 'What is your user name?',
           name: 'username',
-         },
+             },
         {
          type: 'input',
-          message: 'What is your user name?',
-          name: 'username',
-         },
+          message: 'What is this project about?',
+          name: 'project_info',
+             },
          {
          type: 'input',
-          message: 'What is your user name?',
-          name: 'username',
-         },
-        ])
+          message: 'What are the steps to install your project?',
+          name: 'installation',
+             },
+         {
+          type: 'input',
+          message: 'How can we use your project?',
+          name: 'usage',
+             },
+               {
+         type: 'input',
+          message: 'What is your GitHub username?',
+          name: 'credits',
+             },
+         {
+          type: 'list',
+          message: 'What license did you use?',
+          name: 'license',
+          choices: ['MIT', 'Apache', 'PDDL', 'MPL']
+             },
+                {
+         type: 'input',
+          message: 'What features does your project have?',
+          name: 'features',
+             },
             //return a response and use fs to append it
-            .then((data) => {
-                console.log(data)
-            })
+           
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(README, data) {
     fs.writeFile(generateMarkdown(data))
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(answers => {
+        fs.writeFileSync('./dist/generatedReadme.md', generateMarkdown(answers));
+    })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,16 +2,30 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
     let licenseString = ""
-      if(licenseType === "MIT") {
-  licenseString = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+
+      if(license === "MIT") {
+        licenseString = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
       };
+
+      if(license === "Apache") {
+        licenseString = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+            };
+
+      if(license === "PDDL") {
+        licenseString = `[![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)`
+      };
+     
+      if(license === "MPL") {
+        licenseString = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+      };
+
       return licenseString
 };
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  
+
 }
 
 // TODO: Create a function that returns the license section of README
@@ -20,7 +34,35 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.project_name}
+
+ ## Description
+  ${data.project_info}
+
+ ## Table of Contents
+  - [Insallation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+
+ ## Installation
+  ${data.installation}
+
+ ## Usage
+  ${data.usage}
+
+ ## Credits
+  [${data.credits}](https://github.com/${data.credits})
+
+ ## License
+  ${renderLicenseBadge(data.license)}
+
+ ## Features
+  ${data.features}
+
+  
+
+
 
 `;
 }
